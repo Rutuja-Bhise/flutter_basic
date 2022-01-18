5d32bbd26b1176d65e170b57ac507ff0208c1cf4
Rushikesh <rushikeshnarwade53@gmail.com>
2022-01-18 08:44:58 +0530

    Done some changes and added some comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/pages/homepage.dart';
 import 'package:flutter_application/pages/loginpage.dart';
 import 'package:flutter_application/util/routes.dart';
-void main(){
-  runApp(MyApp());
+
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp ({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      routes:{
-        "/":(Context)=>loginpage(),
-        MyRoutes.homeRoute:(context)=>homepage(),
-        MyRoutes.loginRoute:(context)=>loginpage()
-
-
+      routes: {
+        "/": (context) => const LoginPage(),
+        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage()
       },
     );
   }
