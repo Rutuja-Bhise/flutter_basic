247fe056646171d67d1797338b34b47a66982157
Rutuja Bhise <rutujabhise01@gmail.com>
2022-01-20 20:29:36 +0530

    new updation

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/pages/homepage.dart';
-import 'package:flutter_application/pages/loginpage.dart';
-import 'package:flutter_application/util/routes.dart';
+import 'package:flutter_application/pages/login_page.dart';
+import 'package:flutter_application/utils/routes.dart';
+import 'pages/home_page.dart';
+import 'widgets/themes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
       routes: {
-        "/": (context) => const LoginPage(),
-        MyRoutes.homeRoute: (context) => const HomePage(),
-        MyRoutes.loginRoute: (context) => const LoginPage()
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
